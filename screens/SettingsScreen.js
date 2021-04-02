5cbbcd063dea2f5bdf9483ec9e7371180d3d06a4
sebastianchoo <sebastian_choo>
2021-04-02 23:07:38 +0800

    Added dispatch from redux store for mode selector without UI change

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { useSelector } from 'react-redux';
+import { Text, View, StyleSheet, Switch } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { lightModeAction, darkModeAction } from "../redux/ducks/accountPref";
 
 export default function SettingsScreen({ navigation }) {
 
-const isDark = useSelector((state) => state.accountPrefs.isDark);
+  const isDark = useSelector((state) => state.accountPrefs.isDark);
+  const dispatch = useDispatch();
+
+  function switchMode() {
+    isDark ? dispatch(lightModeAction()) : dispatch(darkModeAction())
+  }
 
   return (
     <View style={styles.container}>
-      <Text> Setting </Text>
-      <Text> Current Dark Mode is {JSON.stringify(isDark)} </Text>
+      <Text style={styles.label}> Current Dark Mode is {JSON.stringify(isDark)} </Text>
+      <Switch value={isDark} onValueChange={switchMode}/>
     </View>
   );
 }
@@ -19,5 +25,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center", 
     alignItems: "center"
+  },
+  label: {
+    fontWeight: "bold",
+    fontSize: 20,
+    color: "grey",
   }
 })
